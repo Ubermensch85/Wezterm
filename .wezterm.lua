fb7e19b30c1001525686c3950b17107dae66854e
Ubermensch85 <127672496+Ubermensch85@users.noreply.github.com>
2023-03-16 15:02:02 +0100

    Add files via upload
    New colors

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,38 +1,14 @@
--- WezTerm configuration
----------------------------------------------------------------
 
-local cfg_misc = {
-  window_close_confirmation = "NeverPrompt",
-  check_for_updates = false,
-
-  -- Avoid unexpected config breakage and unusable terminal
-  automatically_reload_config = false,
-
-  -- Make sure word selection stops on most punctuations.
-  --
-  -- Note that dot (.) & slash (/) are allowed though for
-  -- easy selection of (partial) paths.
-  selection_word_boundary = " \t\n{}[]()\"'`,;:@│*",
-
-  -- Do not hold on exit by default.
-  -- Because the default 'CloseOnCleanExit' can be annoying when exiting with
-  -- Ctrl-D and the last command exited with non-zero: the shell will exit
-  -- with non-zero and the terminal would hang until the window is closed manually.
-  exit_behavior = "Close", -- NOTE: this is now the default, remove?
+local wezterm = require 'wezterm'
+return {
+  enable_tab_bar=false,
+  force_reverse_video_cursor = true,
+  color_scheme = "FarSide (terminal.sexy)",
+  font_size = 10.5,
+  window_background_image = "",
+  window_background_opacity = 0.7,
+  colors = {  
+	},
+    
 }
-
--- Merge configs and return!
-------------------------------------------
-
-local mytable = require "lib/mystdlib".mytable
-local full_config = mytable.merge_all(
-  cfg_misc,
-  require("cfg_appearance"),
-  require("cfg_fonts"),
-  require("cfg_keys"),
-  require("cfg_mouse"),
-  {} -- so the last table can have an ending comma for git diffs :)
-)
-
-return full_config
 
