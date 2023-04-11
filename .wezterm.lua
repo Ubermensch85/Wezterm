9f9e8c73b2da142e6db21d726f54c4a565af8c73
Ubermensch85 <127672496+Ubermensch85@users.noreply.github.com>
2023-04-11 15:13:18 +0200

    Add files via upload

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,10 +1,9 @@
-
 local wezterm = require 'wezterm'
 return {
   enable_tab_bar=false,
   force_reverse_video_cursor = true,
-  color_scheme = "FarSide (terminal.sexy)",
-  font_size = 10.5,
+  color_scheme = "ayu",
+  font_size = 12.0,
   window_background_image = "",
   window_background_opacity = 0.7,
   colors = {  
